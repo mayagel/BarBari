8cf3a07525411ac677e21d1fe06e7d6ac0f61d8a
Yagel <myagel2@gmail.com>
2018-01-23 18:49:42 +0200

    the changes

--- app/src/main/java/com/example/yagel/barbari/TheBar.java
+++ app/src/main/java/com/example/yagel/barbari/TheBar.java
@@ -1,8 +1,10 @@
 package com.example.yagel.barbari;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -10,10 +12,15 @@ import java.util.Arrays;
 
 public class TheBar extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
 
 private static TextView bar_name1;
 private static TextView bar_des;
+private static TextView bar_p_t;
+private static ImageView bar_img;
+
+private String[] bar_des_l;
+private String[] bar_place;
+private String[] bar_open_t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,24 @@ private static TextView bar_des;
     public void showBar(){
         bar_name1=(TextView) findViewById(R.id.bar_name);
         bar_des=(TextView) findViewById(R.id.bar_des);
+        bar_p_t=(TextView) findViewById(R.id.bar_p_t);
+
+        bar_img=(ImageView) findViewById(R.id.bar_img);
+
+        bar_des_l=getResources().getStringArray(R.array.bar_des);
+        bar_place=getResources().getStringArray(R.array.bar_place);
+        bar_open_t=getResources().getStringArray(R.array.bar_open_t);
+
 
         bar_name1.setText(SearchActivity.bar_name);
+        bar_des.setText(bar_des_l[SearchActivity.index_bar]);
+        bar_p_t.setText(bar_place[SearchActivity.index_bar]+"    "+bar_open_t[SearchActivity.index_bar]);
+
+        if(SearchActivity.index_bar%2==0)
+        bar_img.setImageResource(R.drawable.bar1_img);
+        else
+            bar_img.setImageResource(R.drawable.bar2_img);
+
 /*
         ArrayList<String> arrayBar= new ArrayList<>();
         arrayBar.addAll(Arrays.asList(getResources().getStringArray(R.array.bar_des)));
